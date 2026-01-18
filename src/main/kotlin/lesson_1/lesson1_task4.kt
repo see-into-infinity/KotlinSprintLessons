2188613b2e1c8dbdefaf2aaf7e4e9f388e99c0fb
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-18 14:29:12 +0300

    KS-1-4 solved 4 task

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val length : Long = 40_868_600_000L
+    val age : UByte = 27u
+    val partOfDay : Float = 0.075F
+    val seconds : UShort = 6480u
+    val partOfYear : Double = 2.0547945205479453E-4
+    val apogee : Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
