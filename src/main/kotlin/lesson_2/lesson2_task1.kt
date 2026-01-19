93d4baa2bf06328cfa07ce846155f968cf656680
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-19 19:35:54 +0300

    KS-2-1 solved 1 task

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+    val firstStudentMark : Float = 3.0f
+    val secondStudentMark : Float = 4.0f
+    val thirdStudentMark : Float = 3.0f
+    val fourthStudentMark : Float = 5.0f
+    var amountOfStudents = 4
+
+    var arithmeticMean : Float = (firstStudentMark + secondStudentMark + thirdStudentMark + fourthStudentMark) / amountOfStudents
+
+    println(arithmeticMean)
+}
