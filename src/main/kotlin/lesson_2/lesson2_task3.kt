e92c502408914a22609fcfccf25a09fd8dfd96b4
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-19 22:18:57 +0300

    KS-2-3 solved 3 task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -8,6 +8,6 @@ fun main() {
     val totalMinutesFromStart = departureTimeHour * 60 + departureTimeMinute + routeTimeInMinutes
     val arrivalHour = (totalMinutesFromStart / 60) % 24
     val arrivalMinute = totalMinutesFromStart % 60
-    
+
     println("%02d:%02d".format(arrivalHour, arrivalMinute))
 }
