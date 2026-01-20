bcd4c6d0c23da828e32a9708a669c4eba8c18b2b
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-20 19:43:39 +0300

    KS-2-3 added variable sixtySeconds

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,11 +2,12 @@ package org.example.lesson_1
 
 fun main() {
     val seconds : Short = 6480
+    val sixtySeconds: Int = 60
 
-    val minutes = seconds / 60
-    val hours = minutes / 60
-    val minutesLeft = minutes % 60
-    val secondsLeft = seconds % 60
+    val minutes = seconds / sixtySeconds
+    val hours = minutes / sixtySeconds
+    val minutesLeft = minutes % sixtySeconds
+    val secondsLeft = seconds % sixtySeconds
 
     println("%02d:%02d:%02d".format(hours, minutesLeft, secondsLeft))
 }
