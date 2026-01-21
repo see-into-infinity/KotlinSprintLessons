942717c1799661f5c1f48006b9703a6222cae549
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-21 19:16:21 +0300

    KS-2-3 fixed 3 task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_2
 
+const val SIXTY_SECONDS = 60
+
 fun main() {
     var departureTimeHour: Int = 9
     var departureTimeMinute: Int = 39
     var routeTimeInMinutes: Int = 457
-    val sixtySeconds: Int = 60
     val hoursInDay: Int = 24
 
-    val totalMinutesFromStart = departureTimeHour * sixtySeconds + departureTimeMinute + routeTimeInMinutes
-    val arrivalHour = (totalMinutesFromStart / sixtySeconds) % hoursInDay
-    val arrivalMinute = totalMinutesFromStart % sixtySeconds
+    val totalMinutesFromStart = departureTimeHour * SIXTY_SECONDS + departureTimeMinute + routeTimeInMinutes
+    val arrivalHour = (totalMinutesFromStart / SIXTY_SECONDS) % hoursInDay
+    val arrivalMinute = totalMinutesFromStart % SIXTY_SECONDS
 
     println("%02d:%02d".format(arrivalHour, arrivalMinute))
 }
