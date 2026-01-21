011c39d189e5f9739a2dcfe517377652a0b6c0a1
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-21 16:02:19 +0300

    KS-3-3 solved 3 task

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+    val inputNumber = 3
+
+    println("""
+        ${inputNumber} x 1 = ${inputNumber * 1}
+        ${inputNumber} x 2 = ${inputNumber * 2}
+        ${inputNumber} x 3 = ${inputNumber * 3}
+        ${inputNumber} x 4 = ${inputNumber * 4}
+        ${inputNumber} x 5 = ${inputNumber * 5}
+        ${inputNumber} x 6 = ${inputNumber * 6}
+        ${inputNumber} x 7 = ${inputNumber * 7}
+        ${inputNumber} x 8 = ${inputNumber * 8}
+        ${inputNumber} x 9 = ${inputNumber * 9}
+        ${inputNumber} x 10 = ${inputNumber * 10}
+    """.trimIndent())
+}
