2b2ce9a9b8f46f74fc3705e96b7daa7520c0c02b
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-21 15:56:08 +0300

    KS-3-2 solved 2 task

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val girlName = "Татьяна"
+    val lastNameBeforeMarriage = "Андреева"
+    val girlMiddleName = "Сергеевна"
+
+    val lastNameAfterMarriage = "Сидорова"
+
+    val ageBeforeMarriage = 20
+    val ageAfterMarriage = 22
+
+    println("$lastNameBeforeMarriage $girlName $girlMiddleName, $ageBeforeMarriage")
+    println("$lastNameAfterMarriage $girlName $girlMiddleName, $ageAfterMarriage")
+}
