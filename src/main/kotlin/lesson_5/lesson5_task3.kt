b4186d82787f0d9f6ba24a82fc2bb08932f8b2a6
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-25 14:57:01 +0300

    KS-5-3 solved 3 task

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_5
+
+import kotlin.random.Random
+
+const val FIRST_WINNING_NUMBER = 7
+const val SECOND_WINNING_NUMBER = 21
+fun main() {
+
+    println("Введите первое число от 0 до 42:")
+    val userFirstInputNumber = readln().toInt()
+
+    println("Введите второе число от 0 до 42:")
+    val userSecondInputNumber = readln().toInt()
+
+    println("Правильные числа для победы: $FIRST_WINNING_NUMBER и $SECOND_WINNING_NUMBER")
+
+    var countCorrectNumbers = 0
+    if(FIRST_WINNING_NUMBER == userFirstInputNumber || SECOND_WINNING_NUMBER == userFirstInputNumber) {
+         countCorrectNumbers++
+    }
+    if(SECOND_WINNING_NUMBER == userSecondInputNumber || FIRST_WINNING_NUMBER == userSecondInputNumber) {
+        countCorrectNumbers++
+    }
+    when (countCorrectNumbers) {
+        2 -> println("Поздравляем! Вы выиграли главный приз!")
+        1 -> println("Вы выиграли утешительный приз!")
+        0 -> println("Неудача!")
+    }
+}
