7b8f116b0d0c84ca69601bbd5c3cb409358c24c9
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-26 09:07:27 +0300

    KS-5-3 fixed 3 task

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -3,23 +3,23 @@ package org.example.lesson_5
 
 const val FIRST_WINNING_NUMBER = 7
 const val SECOND_WINNING_NUMBER = 21
+const val INITIAL_NUMBER_IN_RANGE = 0
+const val END_NUMBER_IN_RANGE = 42
 fun main() {
 
-    println("Введите первое число от 0 до 42:")
+    println("Введите первое число от $INITIAL_NUMBER_IN_RANGE до $END_NUMBER_IN_RANGE:")
     val userFirstInputNumber = readln().toInt()
 
-    println("Введите второе число от 0 до 42:")
+    println("Введите второе число от $INITIAL_NUMBER_IN_RANGE до $END_NUMBER_IN_RANGE:")
     val userSecondInputNumber = readln().toInt()
 
     println("Правильные числа для победы: $FIRST_WINNING_NUMBER и $SECOND_WINNING_NUMBER")
 
-    var countCorrectNumbers = 0
-    if (FIRST_WINNING_NUMBER == userFirstInputNumber || SECOND_WINNING_NUMBER == userFirstInputNumber) {
-        countCorrectNumbers++
-    }
-    if (SECOND_WINNING_NUMBER == userSecondInputNumber || FIRST_WINNING_NUMBER == userSecondInputNumber) {
-        countCorrectNumbers++
-    }
+    val userNumbers = setOf(userFirstInputNumber, userSecondInputNumber)
+    val winningNumbers = setOf(FIRST_WINNING_NUMBER, SECOND_WINNING_NUMBER)
+
+    val countCorrectNumbers = userNumbers.intersect(winningNumbers).size
+
     when (countCorrectNumbers) {
         2 -> println("Поздравляем! Вы выиграли главный приз!")
         1 -> println("Вы выиграли утешительный приз!")
