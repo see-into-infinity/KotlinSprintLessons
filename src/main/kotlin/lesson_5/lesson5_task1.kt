c13217ad5497a5c8d648b23b6a2e8c574d3ce30b
ivsidorenko <sidorenko.evan@yandex.ru>
2026-01-26 09:19:26 +0300

    KS-5-1 fixed 1 task

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,10 +1,11 @@
 package org.example.lesson_5
 
-import kotlin.random.Random
+const val INITIAL_NUMBER_IN_RANGE = 1
+const val END_NUMBER_IN_RANGE = 10
 
 fun main() {
-    val firstRandomNumber = Random.nextInt(1, 11)
-    val secondRandomNumber = Random.nextInt(1, 11)
+    val firstRandomNumber = (INITIAL_NUMBER_IN_RANGE..END_NUMBER_IN_RANGE).random()
+    val secondRandomNumber = (INITIAL_NUMBER_IN_RANGE..END_NUMBER_IN_RANGE).random()
     val correctNumber = firstRandomNumber + secondRandomNumber
 
     println("Решите задачу. Сколько будет $firstRandomNumber + $secondRandomNumber?")
